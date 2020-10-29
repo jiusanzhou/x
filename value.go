a6d5ef278c8340cea79b8f5d8390642719a3d0a6
Zoe <hi@zoe.im>
2020-10-29 20:21:03 +0800

    :tada: add expression

---
+++ value.go
@@ -0,0 +1,97 @@
+package x
+
+import "reflect"
+
+// Value ...
+type Value struct {
+	val interface{}
+
+	cond bool
+}
+
+// NewValue create the value for expression
+func NewValue(v interface{}) *Value {
+	return &Value{
+		val:  v,
+		cond: true,
+	}
+}
+
+// V for simple to create Value like
+func V(v interface{}) *Value {
+	return NewValue(v)
+}
+
+// auto generate the type gen
+
+// String ...
+func (v *Value) String() (string, bool) {
+	vv, ok := v.val.(string)
+	return vv, ok
+}
+
+// Bool ...
+func (v *Value) Bool() (bool, bool) {
+	vv, ok := v.val.(bool)
+	return vv, ok
+}
+
+// Int ...
+func (v *Value) Int() (int, bool) {
+	vv, ok := v.val.(int)
+	return vv, ok
+}
+
+// Int32 ...
+func (v *Value) Int32() (int32, bool) {
+	vv, ok := v.val.(int32)
+	return vv, ok
+}
+
+// Int64 ...
+func (v *Value) Int64() (int64, bool) {
+	vv, ok := v.val.(int64)
+	return vv, ok
+}
+
+// Float32 ...
+func (v *Value) Float32() (float32, bool) {
+	vv, ok := v.val.(float32)
+	return vv, ok
+}
+
+// Float64 ...
+func (v *Value) Float64() (float64, bool) {
+	vv, ok := v.val.(float64)
+	return vv, ok
+}
+
+// Interface ...
+func (v *Value) Interface() interface{} {
+	return v.val
+}
+
+// ============= the scope =================
+
+// Or Value(a).Or(-1)
+func (v *Value) Or(r interface{}) *Value {
+	// check if is else
+	// check if val is nil or zero value
+	if !v.cond || v.val == nil || reflect.ValueOf(v.val).IsZero() {
+		v.val = r
+	}
+	return v
+}
+
+// If Value(a).If(a == 1).Or(0)
+// can accept a func() bool
+func (v *Value) If(r bool) *Value {
+	v.cond = r
+	return v
+}
+
+// Ifn can accept a function: func() bool
+func (v *Value) Ifn(fn func() bool) *Value {
+	v.cond = fn()
+	return v
+}
---
+++ value_test.go
@@ -0,0 +1,33 @@
+package x
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewValue(t *testing.T) {
+	tests := []struct {
+		name string
+		v    *Value
+		want interface{}
+	}{
+		{"Or: Int 0", V(1).Or(-1), 1},
+		{"Or: Int 1", V(0).Or(-1), -1},
+		{"Or: Str empty", V("").Or("empty"), "empty"},
+		{"Or: Str value", V("ok").Or("empty"), "ok"},
+		{"Or: Object nil", V(interface{}(nil)).Or(1), 1},
+		{"IfOr: Int 0", V(1).If(true).Or(-1), 1},
+		{"IfOr: Int 1", V(0).If(false).Or(-1), -1},
+		{"IfOr: Str empty", V("ok").If(false).Or("empty"), "empty"},
+		{"IfOr: Str value", V("ok").If(true).Or("empty"), "ok"},
+		{"IfnOr: Int 0", V(1).Ifn(func() bool { return true }).Or(-1), 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.v.Interface(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
