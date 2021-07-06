21475813c30acceb5dbf9144e1dfab871489944a
Zoe <hi@zoe.im>
2021-07-06 09:17:53 +0800

    :tada: add lb

--- httputil/util.go
+++ httputil/util.go
@@ -1,2 +1,28 @@
 // Package httputil contains lots of helper functions
 package httputil
+
+import "net/http"
+
+// CloneRequest creates a shallow copy of the request along with a deep copy of the Headers.
+func CloneRequest(req *http.Request) *http.Request {
+	r := new(http.Request)
+
+	// shallow clone
+	*r = *req
+
+	// deep copy headers
+	r.Header = CloneHeader(req.Header)
+
+	return r
+}
+
+// CloneHeader creates a deep copy of an http.Header.
+func CloneHeader(in http.Header) http.Header {
+	out := make(http.Header, len(in))
+	for key, values := range in {
+		newValues := make([]string, len(values))
+		copy(newValues, values)
+		out[key] = newValues
+	}
+	return out
+}
