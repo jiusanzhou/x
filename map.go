58baa1d07fd609325543a397213c145c72d86811
Xiaolu Zhou <hi@zoe.im>
2024-12-16 12:41:16 +0800

    :zap: support range for map

--- map.go
+++ map.go
@@ -9,6 +9,24 @@ func Keys[K comparable, V any](maps map[K]V) []K {
 	return items
 }
 
+// Values return all values of map
+func Values[K comparable, V any](maps map[K]V) []V {
+	items := []V{}
+	for _, v := range maps {
+		items = append(items, v)
+	}
+	return items
+}
+
+// Range calls f sequentially for each key and value present in the map. If f returns false, range stops the iteration.
+func Range[K comparable, V any](maps map[K]V, f func(K, V) (shouldContinue bool)) {
+	for k, v := range maps {
+		if !f(k, v) {
+			return
+		}
+	}
+}
+
 // UpdateMap update the map and return deleted
 // , IN map[K]V1 | []V1
 func UpdateMap[K comparable, V1 any, V2 any](
